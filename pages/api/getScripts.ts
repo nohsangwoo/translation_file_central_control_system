36044f5adc4b36d69bb3a4a79481e122a3a4cd2f
nohsangwoo <nsgr12@naver.com>
2022-09-07 18:08:40 +0900

    select platform

--- pages/api/getScripts.ts
+++ pages/api/getScripts.ts
@@ -2,8 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const fs = require('fs')
-import gotalk_ko from './../../public/lang/gotalk/ko.json'
-import gotalk_en from './../../public/lang/gotalk/en.json'
 
 export type getScriptResult = {
   result: string
@@ -13,26 +11,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<getScriptResult>,
 ) {
-  // const objectEntries = Object.entries(gotalk_ko)
-
-  const countrys = [
-    'ar',
-    'de',
-    'en',
-    'es',
-    'fr',
-    'ja',
-    'ko',
-    'pt-BR',
-    'ru',
-    'tr',
-    'vi',
-    'zh_TW',
-    'zh',
-  ]
-
-  console.log('req query: ', req.query)
-
   type query = string | string[] | undefined
   const getScript = (platform: query, country: query) => {
     const importScript = (p: query, c: query) => {
@@ -40,11 +18,10 @@ export default function handler(
         fs.readFileSync(`public/lang/${p}/` + `${c}.json`).toString(),
       )
     }
-    return Object.entries(importScript(req.query.platform, req.query.country))
+    return Object.entries(importScript(platform, country))
   }
 
-  const objectEntries = getScript('gotalk', 'zh')
-  // console.log('objectEntries: ', objectEntries)
+  const objectEntries = getScript(req.query.platform, req.query.country)
 
   const result = objectEntries.map(data => {
     const [key, value] = data
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,30 +4,63 @@ import axios from 'axios'
 import CompanyTable from 'components/companyinfoTable/CompanyTable.client'
 import { dehydrate, useQuery } from '@tanstack/react-query'
 import { queryClient } from 'src/api'
+import { useState } from 'react'
 
-export const onFetch = async () => {
+export const onFetch = async (platform: string) => {
   const result = await axios.get(
-    `http://localhost:3000/api/getScripts?platform=${'gotalk'}&country=${'ko'}`,
+    `http://localhost:3000/api/getScripts?platform=${platform}&country=${'ko'}`,
   )
   return JSON.parse(result.data.result)
 }
 const Home = (
   props: InferGetServerSidePropsType<typeof getServerSideProps>,
 ) => {
-  const { data, refetch, isLoading } = useQuery(['getScript'], onFetch)
+  const [platform, setPlatform] = useState('videohelpme')
+  const platforms = ['videohelpme', 'gotalk']
 
-  console.log('fetching scriptsdata : ', data)
+  const { data, refetch, isLoading } = useQuery(['script', platform], () =>
+    onFetch(platform),
+  )
+
+  const countrys = [
+    'ko',
+    'en',
+    'ar',
+    'de',
+    'es',
+    'fr',
+    'ja',
+    'pt-BR',
+    'ru',
+    'tr',
+    'vi',
+    'zh_TW',
+    'zh',
+  ]
 
   return (
     <div className={styles.container}>
+      <select
+        onChange={e => {
+          console.log(e.target.value)
+          setPlatform(e.target.value)
+        }}
+      >
+        {platforms.map((data, index) => {
+          return (
+            <option key={index} value={data} selected={index === 0}>
+              {data}
+            </option>
+          )
+        })}
+      </select>
       {data && <CompanyTable scripts={data} />}
-      <button onClick={onFetch}>fetch data</button>
     </div>
   )
 }
 
 export async function getServerSideProps() {
-  await queryClient.prefetchQuery(['getScript'], () => onFetch())
+  await queryClient.prefetchQuery(['getScript'], () => onFetch('videohelpme'))
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
